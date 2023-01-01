f4f237fe53ad6b1a4b328f1259da9359185cb839
premambodkar <prem.ambodkar@gmail.com>
2023-01-01 15:05:25 +0530

    1 file modified

--- src/Second.js
+++ src/Second.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Third from './Third.js';
 
 const Second = (props) => {
   return (
     <>
       <div>Second</div>
       <div>Component</div>
+      <Third />
     </>
   );
 };
